703b9210917c0ea8df59b4a0fdc8b7b2679e9a8a
GwangYong Lee <rhkdrmfh@gmail.com>
2019-07-11 12:34:36 -0400

    Add .xcodeproj, Test, example

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by United Merchant Services.inc on 7/11/19.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
